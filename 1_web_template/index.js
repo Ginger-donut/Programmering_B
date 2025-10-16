a65340c8ebdee5296507affc0c0f858ec536c319
Asta Aqraou <astaaaqraou@icloud.com>
2025-10-16 20:32:31 +0200

    first commit

---
+++ 1_web_template/index.js
@@ -0,0 +1,31 @@
+var currentPage = '#page7'
+
+//P5 setup () bliver kladt En gang før siden vises 
+function setup() {
+    console.log('P5 setup kaldt Allahuakbar')
+    //Sæt menu op
+    //Hent alle sider som et array
+    var allPages = selectAll('.page')
+    //løb listen igennem en for en 
+    allPages.map (
+        page => {
+            //Lav et nyt <a> element
+            var menuItem = createElement('a')
+            //Sæt a taggets html til sidens titel
+            menuItem.html(page.attribute('title'))
+            //Sæt eventlistener på a tagget
+            menuItem.mousePressed(
+                ()=>shiftPage('#' + page.attribute('id'))
+            )
+            //Sæt a tagget ind i siddebaren
+            select('.sidebar').child(menuItem)
+        }
+    )
+    
+}
+
+function shiftPage(newPage) {
+    select(currentPage).removeClass('show');
+    select(newPage).addClass('show');
+    currentPage = newPage;
+}
