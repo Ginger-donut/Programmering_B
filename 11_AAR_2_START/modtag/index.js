52cd2c10c0a6dcde6d969a5963cacf4a38c3ab56
Asta Aqraou <astaaaqraou@icloud.com>
2026-08-16 22:53:11 +0200

    lommeregner

--- 11_AAR_2_START/modtag/index.js
+++ 11_AAR_2_START/modtag/index.js
@@ -1,8 +1,9 @@
 var client
-var topic = 'ass/regnestykke' 
+var topic = 'ass/regnestykke'
 
 function setup(){
     noCanvas()
+    //mqtt er et objekt vi får fra mqtt bilbioteket i html siden
     client = mqtt.connect('wss://mqtt.nextservices.dk')
 
     client.on('connect', () => {
@@ -16,21 +17,21 @@ function setup(){
             var svar = beregn(msg)
 
             select('#exprMsg').elt.textContent = msg
-            select('#svarMsg').elt.textContent = (svar === null) ? 'Ugyldigt regnestykke' : svar
+            select('#svarMsg').elt.textContent = (svar === null) ? 'Ugyldigt regnestykke' : svar // viser resultatet af regnestykket eller en fejlmeddelelse
 
             visToast('Nyt regnestykke: ' + msg)
         }
     })
 }
 
-function beregn(udtryk){
-    if(!/^[0-9+\-*/(). ]+$/.test(udtryk)) return null
+function beregn(udtryk){ 
+    if(!/^[0-9+\-*/(). ]+$/.test(udtryk)) return null // stopper hvis der er andet end tal/regnetegn i beskeden
     try {
-        var resultat = Function('"use strict"; return (' + udtryk + ')')()
-        if(typeof resultat !== 'number' || !isFinite(resultat)) return null
+        var resultat = Function('"use strict"; return (' + udtryk + ')')() // beregner regnestykket ved hjælp af Function constructor
+        if(typeof resultat !== 'number' || !isFinite(resultat)) return null // stopper hvis resultatet ikke er et tal
         return resultat
     } catch(e){
-        return null
+        return null // stopper hvis der er en fejl i regnestykket
     }
 }
 
--- 11_AAR_2_START/send/index.js
+++ 11_AAR_2_START/send/index.js
@@ -3,6 +3,7 @@ var topic = 'ass/regnestykke'
 
 function setup(){
     noCanvas()
+    //mqtt er et objekt vi får fra mqtt bilbioteket i html siden
     client = mqtt.connect('wss://mqtt.nextservices.dk')
 
     client.on('connect', () => {
@@ -12,12 +13,12 @@ function setup(){
     select('#sendBtn').mousePressed(()=>{
         var udtryk = select('#exprInput').value()
         if(udtryk){
-            client.publish(topic, udtryk, { retain: true })
+            client.publish(topic, udtryk, { retain: true }) // sender regnestykket til MQTT serveren og beholder det som den sidste besked på topicet
             visToast('Sendte: ' + udtryk)
             select('#exprInput').value('')
 
             setTimeout(()=>{
-                window.location.href = '../modtag/index.html'
+                window.location.href = '../modtag/index.html' // skifter til modtag/index.html efter 300ms
             }, 300)
         }
     })
